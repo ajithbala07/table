d0fc66c69df57d7c9f2b0d001082bd2f473972b4
ajithbala07 <81035659+ajithbala07@users.noreply.github.com>
2021-03-20 22:05:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import List from './Forms';
+import Lists from './Formstyle';
+import Tables from './Formtable';
+
+function App() {
+  return (
+    <div >
+     <Lists />
+      
+    </div>
+  );
+}
+
+export default App;
